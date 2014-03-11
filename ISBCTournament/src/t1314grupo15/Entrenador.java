10f2bc150f926a46f15f4aa40bee3771c7369516
rchampa <ricardocb48@gmail.com>
2014-03-11 15:56:39 +0100

    Entrenador sin cbr

--- ISBCTournament/src/t1314grupo15/Entrenador.java
+++ ISBCTournament/src/t1314grupo15/Entrenador.java
@@ -1,11 +1,5 @@
 package t1314grupo15;
 
-import static jcolibri.util.CopyUtils.copyCaseComponent;
-import jcolibri.cbrcore.CBRCase;
-import jcolibri.soccerbots.GameDescription;
-import jcolibri.soccerbots.GameSolution;
-import jcolibri.soccerbots.Recomendacion;
-import jcolibri.soccerbots.Recomender;
 import teams.ucmTeam.Behaviour;
 import teams.ucmTeam.RobotAPI;
 import teams.ucmTeam.TeamManager;
@@ -27,11 +21,6 @@ public class Entrenador extends TeamManager {
 	private int diferencia_goles_act=0;
 	
 	
-	private GameDescription consulta_ant;
-	private Recomendacion recomendacion_ant;
-	
-	private Recomender recomender = new Recomender();
-	
 	private int num_nuevo_caso;
 
 	private boolean cbr_activado = true;
@@ -48,43 +37,43 @@ public class Entrenador extends TeamManager {
 	public void onTakeStep() {
 	
 		
-		if(cbr_activado){
-		
-			RobotAPI capitan =_players[0].getRobotAPI();
-			
-			tiempo_actual = capitan.getMatchTotalTime()-capitan.getMatchRemainingTime();
-			
-			if((tiempo_actual-tiempo_ultima_consulta)>=N || hayGol(capitan)){// mayor que n segundos
-				
-				diferencia_goles_act = capitan.getMyScore()-capitan.getOpponentScore();
-				
-				GameDescription consulta = new GameDescription();
-				consulta.setDiferenciaGoles(diferencia_goles_act);
-				consulta.setTercio_actual(calcularTercio(capitan));
-							
-				//Se consulta al CBR una recomendación
-				Recomendacion recomendacion_actual = recomender.iniciar_jcolibri(consulta);
-				estrategia = recomendacion_actual.getStrategia();
-				
-				re_asignar_estrategia();
-				
-				//Si existe una consulta(recomendacion) anterior, entonces se aprende (pra bien o para mal)
-				if(consulta_ant!=null){
-					aprendizaje(recomendacion_ant);
-				}
-				else{
-					//Solo entra aquí la primera que se invoca a jcolbri
-					num_nuevo_caso = recomender.getNumCasos()+1; 
-				}
-							
-				diferencia_goles_ant = diferencia_goles_act;
-				consulta_ant = consulta;
-				recomendacion_ant = recomendacion_actual;
-				
-				tiempo_ultima_consulta = tiempo_actual;
-			}
-			
-		}
+//		if(cbr_activado){
+//		
+//			RobotAPI capitan =_players[0].getRobotAPI();
+//			
+//			tiempo_actual = capitan.getMatchTotalTime()-capitan.getMatchRemainingTime();
+//			
+//			if((tiempo_actual-tiempo_ultima_consulta)>=N || hayGol(capitan)){// mayor que n segundos
+//				
+//				diferencia_goles_act = capitan.getMyScore()-capitan.getOpponentScore();
+//				
+//				GameDescription consulta = new GameDescription();
+//				consulta.setDiferenciaGoles(diferencia_goles_act);
+//				consulta.setTercio_actual(calcularTercio(capitan));
+//							
+//				//Se consulta al CBR una recomendación
+//				Recomendacion recomendacion_actual = recomender.iniciar_jcolibri(consulta);
+//				estrategia = recomendacion_actual.getStrategia();
+//				
+//				re_asignar_estrategia();
+//				
+//				//Si existe una consulta(recomendacion) anterior, entonces se aprende (pra bien o para mal)
+//				if(consulta_ant!=null){
+//					aprendizaje(recomendacion_ant);
+//				}
+//				else{
+//					//Solo entra aquí la primera que se invoca a jcolbri
+//					num_nuevo_caso = recomender.getNumCasos()+1; 
+//				}
+//							
+//				diferencia_goles_ant = diferencia_goles_act;
+//				consulta_ant = consulta;
+//				recomendacion_ant = recomendacion_actual;
+//				
+//				tiempo_ultima_consulta = tiempo_actual;
+//			}
+//			
+//		}
 		
 	}
 	
@@ -113,24 +102,24 @@ public class Entrenador extends TeamManager {
 	 * Finalmente se invoca a la funciona aprender del recomendador.
 	 * @param recomendacion
 	 */
-	private void aprendizaje(Recomendacion recomendacion){
-		CBRCase casoaprendido = new CBRCase();
-		
-		consulta_ant.setId(""+num_nuevo_caso);
-		casoaprendido.setDescription(copyCaseComponent(consulta_ant));
-		
-		GameSolution sol = new GameSolution();
-		sol.setId(""+num_nuevo_caso);
-		sol.setEstrategia(recomendacion.getStrategia());
-		sol.setValoracion(diferencia_goles_act-diferencia_goles_ant);
-		casoaprendido.setSolution(sol);
-
-		
-		recomender.aprender(casoaprendido);
-		
-		//Se actualiza el que será el nuevo ID de proximo caso que se aprenda
-		num_nuevo_caso++;
-	}
+//	private void aprendizaje(Recomendacion recomendacion){
+//		CBRCase casoaprendido = new CBRCase();
+//		
+//		consulta_ant.setId(""+num_nuevo_caso);
+//		casoaprendido.setDescription(copyCaseComponent(consulta_ant));
+//		
+//		GameSolution sol = new GameSolution();
+//		sol.setId(""+num_nuevo_caso);
+//		sol.setEstrategia(recomendacion.getStrategia());
+//		sol.setValoracion(diferencia_goles_act-diferencia_goles_ant);
+//		casoaprendido.setSolution(sol);
+//
+//		
+//		recomender.aprender(casoaprendido);
+//		
+//		//Se actualiza el que será el nuevo ID de proximo caso que se aprenda
+//		num_nuevo_caso++;
+//	}
 	
 	public int calcularTercio(RobotAPI capitan){
 		int tercio;
